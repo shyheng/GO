697ea9b93a46916eb907991af91b331726f7bc60
shyheng <shy2210278285@aliyun.com>
2021-02-01 04:06:54 +0800

    测试和自动赋值

---
+++ Go_basis/var.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var shy int = 100
+	fmt.Println(shy)
+
+	var shy1 float64 = 3.2
+	fmt.Println(shy1)
+
+	var shy2 string = "gui"
+	fmt.Println(shy2)
+
+	shy3:=5
+	fmt.Println(shy3)
+
+	shy4:="dei"
+	fmt.Println(shy4)
+
+	shy5:=3.5
+	fmt.Println(shy5)
+}
