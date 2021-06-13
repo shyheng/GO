0d6d68b35788e4c2061a0caf92a7ae3318469452
shyheng <shy2210278285@aliyun.com>
2021-06-13 12:44:39 +0800

    go简单鸡兔同笼

---
+++ Go_basis/百钱百鸡.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+
+
+	for cock:=0; cock<=20 ;cock++{
+		for hen:=0; hen<=33;hen++{
+			for chicken:=0;chicken<=100;chicken++ {
+				if cock+hen+chicken==100 && cock*5+hen*3+chicken/3==100 {
+					fmt.Print("公鸡",cock)
+					fmt.Print("母鸡",hen)
+					fmt.Print("小鸡",chicken)
+					fmt.Println()
+				}
+			}
+		}
+	}
+}
